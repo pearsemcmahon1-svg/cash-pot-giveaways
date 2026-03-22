3c4a37df625a4ee9bc6c50ff68beb386f7c93f7d
pearsemcmahon1-svg <pearsemcmahon1@gmail.com>
2026-03-22 19:29:14 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 export default function Home () {
   return (
-    <div>
+    <main
+      style={{
+        background: "#0a0a0a",
+        color: "white",
+        minHeight: "100vh"
+      }}
+      >
       <h1>Cash Pot Giveaways</h1>
-    </div>div>
+    </main>
     );
 }
