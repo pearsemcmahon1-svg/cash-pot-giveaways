f3ce99d8371bc2da73692a89679f2d2c882213bd
pearsemcmahon1-svg <pearsemcmahon1@gmail.com>
2026-03-22 19:14:43 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home () {
   return (
-    <main style={{
-      background: "#0a0a0a,
+    <main 
+      style={{
+      background: "#0a0a0a",
       color: "white",
       minHeight: "100vh",
       display: "flex",
@@ -9,11 +10,12 @@ export default function Home () {
       justifycontent:"center",
       flexDirection: "column",
       fontFamily: "sans-serif"
-      }}>
-<h1 style={{ fontsize: "48px", marginBottom
+      }}
+      >
+<h1 style={{ fontsize: "48px", marginBottom: "10px" }}>
  💰 Cash Pot Giveaways
   </main>h1>
-    <p style={{ fontSize: "20px" , opacity:
+    <p style={{ fontSize: "20px" , opacity: 0.7 }}>
       your raffle platform is now live
       </p>
     </main> 
